23e1fef10abadf42836d819ddb95c28379e9760e
Sohail Anwar <sohailanwar00115@gmail.com>
2025-08-31 01:00:45 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,52 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [value, setValue] = useState("");
+  const [task, settask] = useState("");
+  const [maintask, setmaintask] = useState([]);
 
-  const handleClick = (e) => {
-    setValue(value + e.target.value);
+  const submithandler = (e) => {
+    e.preventDefault();
+    setmaintask([...maintask, { task }]);
+    settask("");
   };
+  const deleteHandler = (index) => {
+  const updatedTasks = maintask.filter((_, i) => i !== index);
+  setmaintask(updatedTasks);
+};
 
-  const handleClear = () => {
-    setValue("");
-  };
+  let showtask = <h2 className='heading'>No task avaliable</h2>;
 
-  const handleCalculate = () => {
-    try {
-      setValue(eval(value).toString());
-    } catch {
-      setValue("Error");
-    }
-  };
+  if (maintask.length > 0) {
+    showtask = maintask.map((t, i) => {
+      return (
+        <li>
+        <div key={i}>
+          <h3>{t.task}</h3>
+        </div>
+        <button onClick={() => deleteHandler(i)}>Delete</button>
+        </li>
+      );
+    });
+  }
 
   return (
-    <div className='container'>
-      <div className="calculator">
-        <form className='form' action="">
-          <div className='display'>
-            <input type="text" value={value} readOnly />
-          </div>
-          <div>
-            <input type="button" value="7" onClick={handleClick} />
-            <input type="button" value="8" onClick={handleClick} />
-            <input type="button" value="9" onClick={handleClick} />
-            <input type="button" value="/" onClick={handleClick} />
-          </div>
-          <div>
-            <input type="button" value="4" onClick={handleClick} />
-            <input type="button" value="5" onClick={handleClick} />
-            <input type="button" value="6" onClick={handleClick} />
-            <input type="button" value="*" onClick={handleClick} />
-          </div>
-          <div>
-            <input type="button" value="3" onClick={handleClick} />
-            <input type="button" value="2" onClick={handleClick} />
-            <input type="button" value="1" onClick={handleClick} />
-            <input type="button" value="-" onClick={handleClick} />
-          </div>
-          <div>
-            <input type="button" value="C" onClick={handleClear} />
-            <input type="button" value="0" onClick={handleClick} />
-            <input type="button" value="=" onClick={handleCalculate} />
-            <input type="button" value="+" onClick={handleClick} />
-          </div>
-        </form>
+    <>
+      <h2>Sohail Todo List for day</h2>
+      <form onSubmit={submithandler}>
+        <input
+          type="text"
+          placeholder='Enter First Your Task '
+          value={task}
+          onChange={(e) => {
+            settask(e.target.value);
+          }}
+        />
+        <button>Add Task</button>
+      </form>
+      <div className='tasktaker'>
+        <ul>{showtask}</ul>
       </div>
-    </div>
+    </>
   );
 };
 
